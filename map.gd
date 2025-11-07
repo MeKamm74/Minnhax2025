6d9e5a1d1decb2d5dc0507bdf0c7e6dcd722d96c
cole-c <cole.e.cummings@gmail.com>
2025-11-06 22:32:02 -0600

    Added 2d Astar grid with obstacles

---
+++ map.gd
@@ -0,0 +1,42 @@
+
+extends Node2D
+
+enum Tile { OBSTACLE, START_POINT, END_POINT }
+
+@onready var base_layer: TileMapLayer = $base_layer
+@onready var terrain_layer: TileMapLayer = $terrain_layer
+
+var _astar = AStarGrid2D.new()
+var tile_size = Vector2i(24, 24)
+
+func _ready():
+	# Region should match the size of the playable area plus one (in tiles).
+	#Above is from a godot tutorial but the I am not sure why the plus one, testing w/o that currently 
+	var map_width = (base_layer.get_used_rect().size.x)
+	var map_height = (base_layer.get_used_rect().size.y)
+	
+	#get_used_rect always returns x,y of the top left corner and the size in w/h
+	#then all other methods use global coords, so we can use top_left_corner + 0 thru width, etc to get populated coordinates
+	var map_data = base_layer.get_used_rect()
+	
+	_astar.region = Rect2i(map_data.position.x, map_data.position.y, map_width, map_height)
+	_astar.cell_size = tile_size
+	_astar.offset = tile_size * 0.5
+	_astar.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	_astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	_astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	_astar.update()
+	
+	for i in range(_astar.region.position.x, _astar.region.end.x):
+		for j in range(_astar.region.position.y, _astar.region.end.y):
+			var pos = Vector2i(i, j)
+			var cell_data = terrain_layer.get_cell_tile_data(pos)
+			if cell_data && cell_data.get_custom_data("Obstacle"):
+				_astar.set_point_solid(pos)
+				print("Point: " + str(pos.x) + ", " + str(pos.y))
+
+
+func get_path_map(start_coords, end_coords):
+	# Example of how to use the path
+	var path = _astar.get_point_path(start_coords, end_coords)
+	return path
