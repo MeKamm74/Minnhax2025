4493d4f8890c8e68f7d420fc5c2099fca03969d7
cole-c <cole.e.cummings@gmail.com>
2025-11-09 20:11:21 -0600

    lil guy runs around with directional animations

--- map.gd
+++ map.gd
@@ -5,8 +5,12 @@ extends Node2D
 @onready var terrain_layer: TileMapLayer = $terrain_layer
 @onready var building_layer: TileMapLayer = $building_layer
 
+@export var enemy_scene: PackedScene
+
 var _astar = AStarGrid2D.new()
 var tile_size = Vector2i(24, 24)
+var base_coords: Vector2i
+var spawner_coords: Array[Vector2i]
 
 func _ready():
 	# Region should match the size of the playable area plus one (in tiles).
@@ -21,6 +25,7 @@ func _ready():
 	_astar.region = Rect2i(map_data.position.x, map_data.position.y, map_width, map_height)
 	_astar.cell_size = tile_size
 	_astar.offset = tile_size * 0.5
+	# Manhattan Heuristic is for 4-directional grid movement, without diagonals, etc
 	_astar.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
 	_astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
 	_astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
@@ -33,7 +38,6 @@ func _ready():
 			var cell_data = terrain_layer.get_cell_tile_data(pos)
 			if cell_data && cell_data.get_custom_data("Obstacle"):
 				_astar.set_point_solid(pos)
-				print("Point: " + str(pos.x) + ", " + str(pos.y) + " is an obstacle")
 				
 	# check which tiles are buildings from the BUILDING LAYER
 	for i in range(_astar.region.position.x, _astar.region.end.x):
@@ -41,16 +45,33 @@ func _ready():
 			var pos = Vector2i(i, j)
 			var cell_data = building_layer.get_cell_tile_data(pos)
 			if cell_data && cell_data.get_custom_data("Base"):
-				#_astar.set_point_solid(pos)
-				# maybe save these locally for spawners? We don't want them solid or units can't walk on them
-				print("Point: " + str(pos.x) + ", " + str(pos.y) + " is a base")
+				# Assumes we will only have one base
+				base_coords = pos
 			if cell_data && cell_data.get_custom_data("City"):
 				#_astar.set_point_solid(pos)
-				# maybe save these locally for spawners? We don't want them solid or units can't walk on them
-				print("Point: " + str(pos.x) + ", " + str(pos.y) + " is a city")
+				spawner_coords.append(pos)
+				
+	spawn_enemy()
 
 
 func get_path_map(start_coords, end_coords):
 	# Example of how to use the path
 	var path = _astar.get_point_path(start_coords, end_coords)
 	return path
+
+func get_base_coords():
+	return base_coords
+
+func get_enemy_path(current_pos):
+	var _start_pos = terrain_layer.local_to_map(current_pos)
+	var path = get_path_map(_start_pos, base_coords)
+	return path.duplicate()
+
+func spawn_enemy() -> void: 
+	var new_enemy = enemy_scene.instantiate()
+	if(spawner_coords.size() > 0):
+		#enemy's ready function is called after add_child
+		var spawner_pos = building_layer.map_to_local(spawner_coords[0])
+		add_child(new_enemy)
+		new_enemy.global_position = spawner_pos
+		new_enemy.set_path_to_base(get_enemy_path(spawner_pos))
---
+++ test_map_assets/enemy.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+@export var _moveSpeed := 0.8
+
+@onready var animate: AnimatedSprite2D = $CharacterBody2D/AnimatedSprite2D
+
+enum State {
+	IDLE, 
+	FOLLOW
+}
+
+enum Direction {
+	IDLE, 
+	UP, 
+	DOWN, 
+	LEFT, 
+	RIGHT
+}
+
+var _state := State.IDLE
+var _path := PackedVector2Array()
+
+func set_path_to_base(new_path: PackedVector2Array) -> void:
+	_path = new_path
+	move_along_path()
+
+func move_along_path() -> void:
+	for i in range(1, _path.size()):
+		assign_animation(_path[i])
+		var tween = create_tween()
+		tween.tween_property(self, "position", _path[i], _moveSpeed)
+		await tween.finished
+	assign_animation(position)
+	
+func assign_animation(target_pos: Vector2) -> void: 
+	var current_pos = position
+	
+	var direction_x = target_pos.x - current_pos.x
+	var direction_y = target_pos.y - current_pos.y
+	
+	if direction_x > 0:
+		animate.play("move_left")
+		animate.flip_h = true
+	elif direction_x < 0:
+		animate.play("move_left")
+		animate.flip_h = false
+	elif direction_y > 0:
+		animate.play("move_down")
+	elif direction_y < 0:
+		animate.play("move_up")
+	else:
+		animate.play("idle")
